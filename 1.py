35706895ac68048ce1a5d043542ace3388f8e0dc
singhsga21081975 <134230181+singhsga21081975@users.noreply.github.com>
2023-12-09 20:48:24 +0000

    commit

--- 1.py
+++ 1.py
@@ -1,13 +1,15 @@
 def find_pair_with_sum(nums, target):
     num_set = set()
-
+    pairs = []
     for num in nums:
-        complement = target - num
-        if complement in num_set:
-            return (complement, num)
+        c = target - num
+        if c in num_set:
+            pairs.append((c,num))
         num_set.add(num)
+    return pairs
 
-    return None
+
+
 
 # Example usage:
 numbers = [1, 2, 3, 4, 5, 6]
@@ -16,6 +18,6 @@ target_sum = 9
 result = find_pair_with_sum(numbers, target_sum)
 
 if result:
-    print(f"Pair with sum {target_sum}: {result[0]} and {result[1]}")
+    print(f"Pair with sum {target_sum}: {result}")
 else:
     print(f"No pair found with sum {target_sum}")
